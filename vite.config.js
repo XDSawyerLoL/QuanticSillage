3340024218c2786d7b1e6c8f49a3fd332e00e2fe
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-22 23:56:47 +0200

    Build standalone ZOON page

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     team:resolve(process.cwd(),'team.html'),
     press:resolve(process.cwd(),'press.html'),
     news:resolve(process.cwd(),'news.html'),
-    pulse:resolve(process.cwd(),'pulse.html'),
+    pulse:resolve(process.cwd(),'pulse.html'),\n    zoon:resolve(process.cwd(),'zoon.html'),
     careers:resolve(process.cwd(),'careers.html'),
     contact:resolve(process.cwd(),'contact.html')
   }}}
